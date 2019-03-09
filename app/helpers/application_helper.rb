7e149c07cb4dd34d997d0e412579c960f1057a69
cecep29 <cecepefendi.c2p@gmail.com>
2019-03-09 17:37:54 +0700

    add generator copyright helper fo layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+
+  def copyright_generator name, msg
+    "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+  end
 end
