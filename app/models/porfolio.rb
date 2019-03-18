a0b453d7f9f8f575016c80049da349876a48b39f
cecep29 <cecepefendi.c2p@gmail.com>
2019-03-18 17:54:57 +0700

    implemented initial design for portfolio index page

--- app/models/porfolio.rb
+++ app/models/porfolio.rb
@@ -16,6 +16,6 @@ class Porfolio < ApplicationRecord
 
   def set_defaults 
     self.main_image ||= Placeholder.image_generator(height: '600', width: '400')
-    self.thumb_image ||= Placeholder.image_generator(height: '350', width: '200')
+    self.thumb_image ||= Placeholder.image_generator(height: '348', width: '225')
   end
 end
